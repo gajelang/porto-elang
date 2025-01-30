e3ca9ff9fc0af180d08dc493e2153a0d33f55a54
elangsamudra <elangsamudra474@gmail.com>
2025-01-30 12:26:31 +0700

    elang v5

--- src/app/components/hero-section.tsx
+++ src/app/components/hero-section.tsx
@@ -3,7 +3,6 @@
 import { motion, useMotionValue } from "framer-motion";
 import { TypeAnimation } from "react-type-animation";
 import Image from "next/image";
-import { ShinyButton } from "./ui/shiny-button";
 
 export function HeroSection() {
   const mouseX = useMotionValue(0);
@@ -102,12 +101,15 @@ export function HeroSection() {
 
         {/* "Let's Connect" Button with Motion Wrapper */}
         <motion.div whileHover={{ scale: 1.05 }}>
-          <ShinyButton
+          <button
             onClick={scrollToContact}
-            className="rounded-full px-6 py-2 md:px-14 md:py-3 md:text-1xl"
+            className="relative inline-flex h-12 overflow-hidden rounded-full p-[1px] focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50"
           >
-            Let&apos;s Connect
-          </ShinyButton>
+            <span className="absolute inset-[-1000%] animate-[spin_2s_linear_infinite] bg-[conic-gradient(from_90deg_at_50%_50%,#E2CBFF_0%,#393BB2_50%,#E2CBFF_100%)]" />
+            <span className="inline-flex h-full w-full cursor-pointer items-center justify-center rounded-full bg-slate-950 px-6 py-2 text-sm font-medium text-white backdrop-blur-3xl">
+              Let&apos;s Connect
+            </span>
+          </button>
         </motion.div>
       </motion.div>
     </section>
